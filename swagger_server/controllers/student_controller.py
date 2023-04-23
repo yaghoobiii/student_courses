e7f78527b5c73f897506d996b1a266ef0a1816a0
yaghoobi <103657696+yaghoobiii@users.noreply.github.com>
2023-04-23 06:09:41 +0430

    2023.04.23 - 2 (deletion of wrong method)

--- swagger_server/controllers/student_controller.py
+++ swagger_server/controllers/student_controller.py
@@ -17,12 +17,5 @@ def login(password):  # noqa: E501
     """
     # TODO
     vet: list[password] = list()
-    vet.append(password("1","مریم یعقوبی","88522000","0"))
-    vet.append(password("2", "سارا یاغی", "88522522", "0"))
-    vet.append(password("3", "امیرحسن زاده", "55886445", "0"))
-    vet.append(password("4", "پارسااسماعیلی", "55669888", "0"))
-    vet.append(password("5", "سمیراآشوری", "31255569", "0"))
-    vet.append(password("6", "حسن کریمی", "555869", "0"))
-    vet.append(password("7", "هدی جنالی زاده","55698455","0"))
 
     return 'vet'

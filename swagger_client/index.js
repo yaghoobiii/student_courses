53c6091d3a59231a55b2c36cc4712c8a8ff160ab
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-04-27 12:22:33 +0330

    2023.04.27 - 6 (a small fix)

--- swagger_client/index.js
+++ swagger_client/index.js
@@ -1,5 +1,5 @@
 $("#login").click(() => {
     $.get("/login", $("#login_form").serialize(), function (data) {
-        alert(data); // show response from the PYTHON script.
+        alert(JSON.stringify(data));
     });
 });

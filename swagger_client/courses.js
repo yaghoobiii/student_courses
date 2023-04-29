025531d9580df19a5607e69d3eff55f2aa8458c8
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-04-29 13:57:17 +0430

    2023.04.29 - 1 (as far as courses.html)

---
+++ swagger_client/courses.js
@@ -0,0 +1,5 @@
+var userId = $.cookie("user_id"), token = $.cookie("user_id");
+if (!$.cookie("user_id") || !$.cookie("token"))
+    location.assign("/");
+
+//....
--- swagger_client/index.js
+++ swagger_client/index.js
@@ -1,5 +1,19 @@
+if ($.cookie("user_id") && $.cookie("token"))
+    signedIn();
+
 $("#login").click(() => {
-    $.get("/login", $("#login_form").serialize(), function (data) {
-        alert(JSON.stringify(data));
+    $.get("/login", $("#login_form").serialize(), function (loginStatus) {
+        if (loginStatus.token == null || loginStatus.user == null) {
+            alert("Wrong username or password!");
+            return;
+        }
+
+        $.cookie("user_id", loginStatus.user.id, {expires: 10});
+        $.cookie("token", loginStatus.token, {expires: 10, secure: true});
+        signedIn();
     });
 });
+
+function signedIn() {
+    location.assign("courses.html");
+}

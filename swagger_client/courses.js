9f0251ce5e3c93a2822c8f88298bc4603200f310
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-04-29 18:59:49 +0430

    2023.04.29 - 4 (courses API)

--- swagger_client/courses.js
+++ swagger_client/courses.js
@@ -1,11 +1,37 @@
-var userId = $.cookie("user_id"), token = $.cookie("user_id");
-if (!$.cookie("user_id") || !$.cookie("token"))
-    location.assign("/");
+var userId = $.cookie("user_id"), token = $.cookie("token");
+if (!userId || !token) location.assign("/");
 
-let main = document.getElementById('SelectAll');
-let select = document.getElementsByClassName('select')
-main.onclick = () => {
-    if (main.checked === true) {
+// Load courses
+const week = {
+    0: "شنبه",
+    1: "یک شنبه",
+    2: "دو شنبه",
+    3: "سه شنبه",
+    4: "چهارشنبه",
+    5: "پنجشنبه",
+    6: "جمعه",
+};
+$.get("/courses", "token=" + token, function (courses) {
+    for (let i = 0; i < courses.length; i++) $("#coursesTBody").append(
+        "    <tr>" +
+        "      <td><input type=\"checkbox\" class=\"select\" name=\"\"></td>" +
+        "      <td>" + courses[i].teacher_name + "</td>" +
+        "      <td>" + courses[i].name + "</td>" +
+        "      <td>" + week[courses[i].weak_day] + "</td>" +
+        "      <td>" + courses[i].start_time + "</td>" +
+        "      <td>" + courses[i].end_time + "</td>" +
+        "    </tr>"
+    )
+    $(".select").change(function () {
+        if (this.checked) {
+            // TODO
+        }
+    });
+});
+
+$("#SelectAll").click(() => {
+    let select = document.getElementsByClassName('select');
+    if ($("#SelectAll").checked === true) {
         for (let i = 0; i < select.length; i++) {
             select[i].checked = true;
             document.querySelector('button').style.display = 'block';
@@ -16,4 +42,4 @@ main.onclick = () => {
             document.querySelector('button').style.display = 'none';
         }
     }
-}
+});

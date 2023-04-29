8874d9939352e99cb56cda0c3b1c1f057450c81c
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-04-29 16:50:20 +0330

    2023.04.29 - 3 (code style fixes)

--- swagger_client/courses.js
+++ swagger_client/courses.js
@@ -2,4 +2,18 @@ var userId = $.cookie("user_id"), token = $.cookie("user_id");
 if (!$.cookie("user_id") || !$.cookie("token"))
     location.assign("/");
 
-//....
+let main = document.getElementById('SelectAll');
+let select = document.getElementsByClassName('select')
+main.onclick = () => {
+    if (main.checked === true) {
+        for (let i = 0; i < select.length; i++) {
+            select[i].checked = true;
+            document.querySelector('button').style.display = 'block';
+        }
+    } else {
+        for (let i = 0; i < select.length; i++) {
+            select[i].checked = false;
+            document.querySelector('button').style.display = 'none';
+        }
+    }
+}
